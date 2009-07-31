018f6c045a0a52b37cfc00a2e68b981e2c2c21ce
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-07-31 19:39:23 +0200

    add dgps_tool to control the board

---
+++ dgps_tool.cc
@@ -0,0 +1,41 @@
+#include "dgps.hh"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void usage()
+{
+    cerr << "usage: dgps_tool <device> <cold-reset|warm-reset|status|almanac>" << endl;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        usage();
+        return 1;
+    }
+
+    string device  = argv[1];
+    string command = argv[2];
+
+    DGPS gps;
+    if (!gps.open(device))
+    {
+        cerr << "cannot open " << device << endl;
+        return 1;
+    }
+
+    if (command == "cold-reset")
+        gps.reset(true);
+    else if (command == "warm-reset")
+        gps.reset(false);
+    else if (command == "status")
+        gps.dumpStatus();
+    else if (command == "almanac")
+        gps.dumpAlmanac();
+    else
+        usage();
+
+}
+
